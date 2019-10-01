00c3d9b47c5fdc9acb48817bcfc305357186535b
greebie <ryan.deschamps@gmail.com>
2019-10-01 12:23:53 -0400

    Add tests to bivariate class.

--- src/test/scala/org/dnvparser/BivariateTest.scala
+++ src/test/scala/org/dnvparser/BivariateTest.scala
@@ -33,7 +33,7 @@ import org.apache.commons.io.FileUtils
 import org.scalatest.{AsyncFunSuite, BeforeAndAfter, Matchers}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import breeze.linalg.{DenseMatrix}
+import breeze.linalg.{DenseMatrix, *}
 import scala.util.Try
 import java.io.File
 import java.nio.file.{Files, Paths}
@@ -44,6 +44,11 @@ import scala.concurrent.Future
 class BivariateTest extends AsyncFunSuite with BeforeAndAfter with Matchers {
   val file = getClass.getResource("/bivariate_sample.dnv").getPath
   val bivariate = Bivariate(file)
+  val tolerance = 0.001 // Floating point precision for tests
+
+  before {
+    bivariate.removeSingle = true
+  }
 
 
   test ("Gets the bivariate file") {
@@ -81,19 +86,56 @@ class BivariateTest extends AsyncFunSuite with BeforeAndAfter with Matchers {
       rowLabels,
       rowLabels,
       expected))
+    bivariate.removeSingle = false
+    test.matrix should equal (bivariate.matrix)
   }
 
   test("Removes Singles") {
     val removed = bivariate.removeSingles()
-    val colLabels = Vector("Asian", "North America", "Superpowers", "Rice")
-    val rowLabels = Vector("China", "India", "US", "Mexico")
-    removed should be (BivariateMatrix(
-      colLabels, colLabels,
+    val rowLabels = Vector("Asian", "North America", "Superpowers", "Rice")
+    val colLabels = Vector("China", "India", "US", "Mexico")
+    val bivMatrix = (BivariateMatrix(
       rowLabels, rowLabels,
+      colLabels, colLabels,
       DenseMatrix(
         (1.0, 1.0, 0.0, 0.0),
         (0.0, 0.0, 1.0, 1.0),
         (1.0, 0.0, 1.0, 0.0),
         (1.0, 1.0, 0.0, 1.0))))
+    removed should be (bivMatrix)
+    bivariate.rowLabels should equal (rowLabels)
+    bivariate.colLabels should equal (colLabels)
+  }
+
+  test("Correspondence Values") {
+
+    val number = 9.0
+    val residual1 = 0.0185185
+    val residual2 = 0.04938
+    val stand1 = -1.2546
+    val stand2 = 0.8164
+    val stand3 = -0.5448
+    val stand4 = 0.6273
+    val dim1 = 0.5378
+    val dim2 = 0.375
+    val profile1 = 1.0
+    val profile2 = 0.0
+    val avg1 = 0.0247
+    val avg2 = 0.03704
+    bivariate.n should equal (number)
+    bivariate.ResidualsSS(0,0) should equal (residual1 +- tolerance)
+    bivariate.ResidualsSS(0,3) should equal (residual2 +- tolerance)
+    bivariate.standCoordsRow(0,0) should equal (stand1 +- tolerance)
+    bivariate.standCoordsRow(0,3) should equal (stand2 +- tolerance)
+    bivariate.standCoordsCol(0,0) should equal (stand3 +- tolerance)
+    bivariate.standCoordsCol(0,3) should equal (stand4 +- tolerance)
+    bivariate.rowCoords(0, 0) should equal (stand1 +- tolerance)
+    bivariate.colCoords(0,0) should equal (stand3 +- tolerance)
+    bivariate.dimensions(0) should equal (dim1 +- tolerance)
+    bivariate.dimensions(1) should equal (dim2 +- tolerance)
+    bivariate.colProfile(1, 2) should equal (profile1 +- tolerance)
+    bivariate.rowProfile(1,1) should equal (profile2 +- tolerance)
+    bivariate.avgColProfile(0) should equal (avg1 +- tolerance)
+    bivariate.avgRowProfile(0) should equal (avg2 +- tolerance)
   }
 }
