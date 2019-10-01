0e69e84be234137af8e505aec459f33180ba0007
greebie <ryan.deschamps@gmail.com>
2019-10-01 11:42:49 -0400

    Comment out test information in App.scala.

--- src/main/scala/org/dnvparser/App.scala
+++ src/main/scala/org/dnvparser/App.scala
@@ -10,21 +10,22 @@ import scala.io.Source
  */
 object App {
   def main(args: Array[String]): Unit = {
-    val bipath = "/Users/ryandeschamps/cybersecurity.dnv"
+    // val bipath = "/Users/ryandeschamps/cybersecurity.dnv"
     val path = getClass.getResource("").getPath
     val inpath = getClass.getResource("/example.dnv").getPath
     val writer = if (args.length < 2) {
-      new PrintWriter(new File(path + "/output"))} else {
+      new PrintWriter(new File(path + "/output"))
+    } else {
         Option(args(1)) match {
-      case Some(x) => new PrintWriter(new File(x))
-      case None => new PrintWriter(new File(path + "/output"))}
+          case Some(x) => new PrintWriter(new File(x))
+          case None => new PrintWriter(new File(path + "/output"))}
     }
     val file = if (args.isEmpty) {inpath} else {Option(args(0)) match {
       case Some(x) => x
       case None => inpath
     }}
     val graph = Network(file)
-    val bigraph = Bivariate(bipath)
+    // val bigraph = Bivariate(bipath)
     // bigraph.plotCoords()
     // bigraph.removeSingle = false
     val eigDegree = graph.eigenVectorCentrality()
